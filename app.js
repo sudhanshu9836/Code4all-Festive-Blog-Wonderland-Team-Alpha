35b6e85926f1bcf7e1756725b4de60988f3de888
Sudhanshu Tiwari <sudhanshutiwari9836@gmail.com>
2023-12-28 15:11:51 +0530

    Updated contacts and functionality of navigation tabs

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+//Navigations
+
+function scroll(id){
+    let element = document.getElementById(id);
+    element.scrollIntoView({
+        behavior:"smooth"
+    });
+}
+document.getElementById("aboutLink").addEventListener("click", function(){
+    scroll("about");
+})
+document.getElementById("contactLink").addEventListener("click", function(){
+    scroll("contact");
+})
+
+//Animations
 gsap.from(".para1, .para2",{
     x: -350,
     duration: 2,
@@ -17,4 +33,13 @@ gsap.from(".abtcontent",{
         trigger: ".abtcontent",
         scroller: "body"       
     }
-})
+});
+gsap.from("#contact-list",{
+    y:250,
+    duration: 1,
+    opacity: 0,
+    scrollTrigger:{
+        trigger: "#contact-list",
+        scroller: "body"       
+    }
+});
